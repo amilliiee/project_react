cb4666995b2f7f05ed25cc771046b4c118bb6107
Elisha Garris <elishagarris@gmail.com>
2024-03-11 02:58:33 -0400

    Added A Main Page with Italian Recipe Cards and A Pages Folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import SearchArea from './components/SearchArea.js';
+import MainPage from './Pages/Main-Page';
+// import SearchArea from './components/SearchArea.js';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <SearchArea/>
-      <h4>Cards with dishes to choose?</h4>
-      <h5>.</h5>
-      <h5>.</h5>
-      <h5>.</h5>
-      <h5>.</h5>
-      <h5>.</h5>
-      <Footer />
+      <MainPage/>
     </div>
   );
 }
--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,13 +1,13 @@
-const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${textInput}`
+// const url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${textInput}`
 
 //Component
-const SearchArea = () => {
-    return (
-        <div>
-            <h1>Search For A Recipe</h1>
-            <input type ="text"></input>
-        </div>
-    )
-}
+// const SearchArea = () => {
+//     return (
+//         <div>
+//             <h1>Search For A Recipe</h1>
+//             <input type ="text"></input>
+//         </div>
+//     )
+// }
 
-export default SearchArea
+// export default SearchArea
