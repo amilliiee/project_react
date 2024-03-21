eb3a66f5bbb6d5fff1264aa49b1bb52bce8d5a9d
Amelia <alsylvester@hotmail.com>
2024-03-20 17:19:07 -0700

    pushing Garris branch to main?

--- src/Pages/Main-Page.js
+++ src/Pages/Main-Page.js
@@ -5,20 +5,9 @@ import RestaurantOverview from "../components/Restaurant Overview"
 const MainPage = () => {
     return (
         <div>
-<<<<<<< HEAD
             <Header />
             <RestaurantOverview/>
             <Footer />
-=======
-            <div>
-                <Header />
-            </div>
-            <RestaurantOverview/>
-            <RestaurantOverview/>
-            <div>
-                <Footer />
-            </div>
->>>>>>> 7b87cf2f48a4d4684c816cf143d36d8936678755
         </div>
     )
 }
--- src/Pages/Search-Page.js
+++ src/Pages/Search-Page.js
@@ -1,20 +1,12 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-<<<<<<< HEAD
 import SearchArea from "../components/SearchArea"
-=======
-import SearchAreaDown from "../components/SearchArea-Unavailable"
->>>>>>> 7b87cf2f48a4d4684c816cf143d36d8936678755
 
 const SearchPage = () => {
     return(
         <div>
             <Header/>
-<<<<<<< HEAD
             <SearchArea/>
-=======
-            <SearchAreaDown/>
->>>>>>> 7b87cf2f48a4d4684c816cf143d36d8936678755
             <Footer/>
         </div>
     )
